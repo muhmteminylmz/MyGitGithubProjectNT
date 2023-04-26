f6d68b03484902d94da2bef79d8125c43d1cac98
e y <muhammed53m@gmail.com>
2023-04-26 22:02:56 +0300

    v4

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -7,6 +7,7 @@ public class AtmProject {
         //version2
         System.out.println("Learn java earn money");
         //version3
-
+        System.out.println("Git ogrenmek cok kolay");
+        //version4
     }
 }
